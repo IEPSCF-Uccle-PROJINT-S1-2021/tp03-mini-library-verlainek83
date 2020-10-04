0c1c1f635de470136db125127b240a8c49c0cc27
PROJINT kove1903 <kove1903@student.iepscf-uccle.be>
2020-10-04 23:26:32 +0200

    creation of table books content's

---
+++ books.js
@@ -0,0 +1,11 @@
+var mesBooks = [
+    ["Guillaume Debré ", "L 'affaire Lafayette", "roman historique"],
+    ["Michael Ende ", "L'Histoire sans fin ", "fantasy "],
+    ["érald Messadié", "La conspiration Jeanne d'Arc ", "roman historique "],
+    ["J.R.R.Tolkien ", "Le Seigneur des anneaux ", "fantasy "],
+    ["Andrzej Sapkowski ", "Le Sorceleur ", " fantasy "],
+    ["George R.R.Martin ", "Le Trône de fer ", " fantasy "],
+    ["Hervé Bazin ", "Vipère au poing ", " autobiographie "],
+    ["Marie Cardinal ", "Les mots pour le dire ", "autobiographie"],
+    ["Giacomo Casanova", "Histoire de ma vie ", "autobiographie "]
+];
