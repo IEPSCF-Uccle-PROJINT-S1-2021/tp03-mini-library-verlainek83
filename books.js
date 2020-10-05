15781b8a08362eea1847b884fe8ee3d9948fd1c5
PROJINT kove1903 <kove1903@student.iepscf-uccle.be>
2020-10-05 22:17:29 +0200

    correction error2

--- books.js
+++ books.js
@@ -1,4 +1,23 @@
-exports.list = function(callback) {
+var mesBooks = [
+    ["Guillaume Debré ", "L 'affaire Lafayette", "roman historique"],
+    ["Michael Ende ", "L'Histoire sans fin ", "fantasy "],
+    ["érald Messadié", "La conspiration Jeanne d'Arc ", "roman historique "],
+    ["J.R.R.Tolkien ", "Le Seigneur des anneaux ", "fantasy "],
+    ["Andrzej Sapkowski ", "Le Sorceleur ", " fantasy "],
+    ["George R.R.Martin ", "Le Trône de fer ", " fantasy "],
+    ["Hervé Bazin ", "Vipère au poing ", " autobiographie "],
+    ["Marie Cardinal ", "Les mots pour le dire ", "autobiographie"],
+    ["Giacomo Casanova", "Histoire de ma vie ", "autobiographie "]
+];
+
+module.exports = {
+    list: function(callback) {
+        return mesBooks;
+    }
+}
+
+
+/*exports.list = function(callback) {
     //for (let i = 0; i < this.length; i++) {
     //console.log(this[i]);
     //}
@@ -21,8 +40,9 @@ exports.list = function(callback) {
     for (var il = 0; il < nbl; il++) {
 
         //alert('Parcours des colonnes de la ligne => ' + il);
+        console.log('------------------------------------------- ');
         console.log('Parcours des colonnes de la ligne => ' + il);
-
+        console.log('------------------------------------------- ');
         for (var ic = 0; ic < nbc; ic++) {
 
             // pacours colonnes
@@ -33,4 +53,4 @@ exports.list = function(callback) {
 
     }
     return 1;
-}
+}*/
